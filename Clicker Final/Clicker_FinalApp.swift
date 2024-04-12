65071adbcc94358656c977e8e056c0a1985553da
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-12 09:33:27 -0500

    Initial Commit

---
+++ Clicker Final/Clicker_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clicker_FinalApp.swift
+//  Clicker Final
+//
+//  Created by Phillip Mantatsky on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Clicker_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
